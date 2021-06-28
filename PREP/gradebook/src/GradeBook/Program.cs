68ce408dd5f8500322c0b98a0e8c2b3cfe49c509
tanviredu <tanviredu2018@gmail.com>
2021-06-28 16:11:09 +0600

    added

---
+++ PREP/gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var book = new Book("Tanvirs GradeBook");
+            book.AddGrade(100.00);
+            book.AddGrade(60.00);
+            book.AddGrade(100.00);
+            book.AddGrade(100.00);
+            book.ShowStatistics();
+            
+
+           
+            
+            
+
+        }
+    }
+}
