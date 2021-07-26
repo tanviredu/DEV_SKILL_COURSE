fb6a82ce13dbe937e5953be2dd73eeffc85cf9f5
tanviredu <tanviredu2018@gmail.com>
2021-07-26 16:04:07 +0600

    added

--- Combining and filtering data with TSQL/PracticeSQL/TanvirQuery.sql
+++ Combining and filtering data with TSQL/PracticeSQL/TanvirQuery.sql
@@ -107,7 +107,93 @@ SELECT O.CustomerID,OD.ProductID,P.ProductName ,O.OrderID FROM Orders O INNER JO
 SELECT * FROM Employees;
 
 -- Find the managers
+-- REMEMBER IT IS A SELF JOIN BECAUSE
+-- MANAGERS IS ALSO EMPLOYEES
 
 SELECT CONCAT_WS(' ',mgrs.FirstName,mgrs.LastName) AS Managers FROM
 Employees emps JOIN Employees mgrs 
 On emps.ReportsTo = mgrs.EmployeeID;
+
+SELECT * FROM Employees;
+
+SELECT * FROM Employees WHERE Title='Sales Representative' OR City='London';
+
+SELECT * FROM Employees WHERE City in ('London');
+
+SELECT * FROM Employees WHERE City not in ('London');
+
+
+--SUB QUERY
+
+SELECT * FROM Orders WHERE OrderID in(SELECT OrderID FROM [Order Details]);
+SELECT CustomerID FROM Orders WHERE OrderID in(SELECT OrderID FROM [Order Details]);
+
+SELECT * FROM Orders ORDER BY OrderID;
+SELECT * FROM Orders ORDER BY OrderID DESC;
+
+SELECT * FROM Products WHERE ProductName LIKE '%c%';
+SELECT * FROM Products WHERE ProductName not LIKE '%c%';
+
+
+-- LIMITING THE RESULTS WITH TSQL FUNCTION
+-- YOU CAN LIMIT THE RESULT WITH THE TOP FUNCTION
+SELECT TOP(10) * FROM Customers;
+SELECT TOP(10) * FROM Orders;
+
+SELECT TOP(5) * FROM Customers;
+SELECT TOP(5) * FROM Orders;
+
+-- FIND THE CUSTOMER NAME AND ID WHO ACTUALLY ORDER SOMETHING
+-- MEANS THE WHOSE CUSTOMER ID IS IN THE ORDER TABLE
+SELECT C.CustomerID,C.ContactName,O.OrderID FROM Customers C JOIN Orders O ON
+C.CustomerID=O.CustomerID;
+
+SELECT TOP(5) * FROM Products;
+SELECT TOP(10) PERCENT *  FROM Customers;
+
+
+-- AGGREGATE DATA IN  A QUERY
+-- SELECT NUMBER IF ROWS IN CUSTOMER AND ORDER AND CUSTOMER
+SELECT COUNT(*) FROM Customers;
+SELECT COUNT(*) FROM Orders;
+SELECT COUNT(*) FROM Products;
+
+SELECT COUNT(*) FROM Orders;
+SELECT DISTINCT COUNT(OrderID) FROM Orders;
+SELECT TOP(2) * FROM Products;
+
+
+SELECT * FROM Products P LEFT JOIN [Order Details] OD 
+ON P.ProductID = OD.ProductID
+LEFT JOIN Orders O 
+ON OD.OrderID = O.OrderID;
+
+
+--UNION
+
+SELECT TOP(3) CompanyName From Customers
+UNION ALL
+SELECT TOP(3) FirstName FROM Employees;
+
+--INTERSECT
+
+SELECt DISTINCT o.OrderID FROM Orders o JOIN [Order Details] od
+ON o.OrderID = od.OrderID;
+
+SELECT OrderID FROM Orders WHERE OrderID IN(SELECT OrderID FROM [Order Details]);
+
+SELECT OrderID FROM Orders
+INTERSECT
+SELECT OrderID FROM [Order Details];
+
+
+--TEMPORARY ABLE
+ -- TMP IS THE TEMPORARY TABLE
+ -- THAT IS CREATED FROM CUSTOMER TABLE
+ -- AFTER RUNNING QUERY 
+ -- WE DROP THE TABLE
+SELECT COUNT(*) AS CUSTOMERCOUNT
+INTO #tmp
+FROM Customers;
+SELECT * FROM #tmp;
+DROP TABLE #tmp;
